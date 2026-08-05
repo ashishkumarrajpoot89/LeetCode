dbd38a9a7a26a2eccf62701b05a203d862c22dc0
ASHISH KUMAR RAJPOOT <106008945+ashishkumarrajpoot89@users.noreply.github.com>
2026-08-05 11:56:00 +0530

    Time: 31 ms (33.3%), Space: 25.8 MB (66.39%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        result = set()
+        for num in nums:
+            if num in result:
+                return True
+            result.add(num)
+        return False
+        
